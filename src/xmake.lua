84e2b9761a05c258152c53ff0992e1f4e213ec44
LYJ <532874387@qq.com>
2025-07-31 00:19:10 +0800

    添加拼音库

--- src/xmake.lua
+++ src/xmake.lua
@@ -2,7 +2,7 @@
 target("my_command")
     set_kind("binary")
     add_files("/*.cpp")
-    add_files("../include/imsearch/*.cpp")
+    add_deps("imsearch")
     after_build(function (target)
         os.cp("./assets", "$(builddir)/$(host)/$(arch)/$(mode)/")
         print("$(builddir)/$(host)/$(arch)/$(mode)/assets")
